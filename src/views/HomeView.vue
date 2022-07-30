28d480861bae72b58896b7b4bf89d6c5b9d8995f
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-07-30 21:59:24 +0700

    refactor: convert to script setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default Vue.extend({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
