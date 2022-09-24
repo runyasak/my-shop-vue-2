883069b6bfc8df2c1b38f2352dcf878900d6345e
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-09-24 16:41:54 +0700

    feat: implement SfImage on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script lang="ts" setup>
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { SfImage } from '@storefront-ui/vue';
 </script>
 
 <template>
   <div class="home">
+    <SfImage
+      src="https://picsum.photos/300/300"
+      alt="Vila stripe maxi shirt dress"
+      :width="null"
+      :height="null"
+    />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
